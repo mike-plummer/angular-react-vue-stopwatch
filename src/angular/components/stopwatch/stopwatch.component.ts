7b6fefb0d999a57ddf2384a830a0ea564cd9af1a
Mike Plummer <mike-plummer@users.noreply.github.com>
2017-07-06 15:20:31 -0500

    Implementing Angular stopwatch

--- src/angular/components/stopwatch/stopwatch.component.ts
+++ src/angular/components/stopwatch/stopwatch.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
+import { Subscription } from 'rxjs/Subscription';
+import { Constants } from '../../../common';
 
 @Component({
   selector: 'stopwatch',
   templateUrl: './stopwatch.component.html',
   styleUrls: ['./stopwatch.component.css']
 })
-export class StopwatchComponent {
+export class StopwatchComponent implements OnDestroy {
 
+  time: number = 0.0;
+  private subscription: Subscription;
+
+  get isRunning(): boolean {
+    return Boolean(this.subscription);
+  }
+
+  start(): void {
+    if (this.isRunning) {
+      return;
+    }
+
+    this.subscription = IntervalObservable.create(Constants.PERIOD).subscribe(() => this.time += Constants.INCREMENT);
+  }
+
+  stop(): void {
+    if (!this.isRunning) {
+      return;
+    }
+
+    this.subscription.unsubscribe();
+    this.subscription = null;
+  }
+
+  reset(): void {
+    if (this.isRunning) {
+      this.stop();
+    }
+    this.time = 0.0;
+  }
+
+  ngOnDestroy(): void {
+    this.stop();
+  }
 }
---
+++ src/common.ts
@@ -0,0 +1,4 @@
+export class Constants {
+  static PERIOD = 10;
+  static INCREMENT = Constants.PERIOD / 1000;
+}
