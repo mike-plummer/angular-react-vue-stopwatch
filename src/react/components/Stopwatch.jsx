b2c2a683cd7b110cb2026e3742be596a1ef5ad80
Mike Plummer <mike-plummer@users.noreply.github.com>
2017-07-06 15:52:59 -0500

    Implementing React stopwatch

--- src/react/components/Stopwatch.jsx
+++ src/react/components/Stopwatch.jsx
@@ -1,17 +1,58 @@
 import React from 'react';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
+
+import { Constants } from '../../common';
 
 export class Stopwatch extends React.Component {
 
+  subscription = null;
+
   constructor() {
     super();
-    this.state = {
-      time: ''
-    };
+  }
+
+  componentWillMount() {
+    this.setTime(0.0);
   }
 
   render() {
-    return <p>
-      React stopwatch!
-    </p>
+    return <div>
+      <p>{ this.state.time.toFixed(2) }</p>
+      <button onClick={this.start.bind(this)} disabled={this.state.active}>Start</button>
+      <button onClick={this.stop.bind(this)} disabled={!this.state.active}>Stop</button>
+      <button onClick={this.reset.bind(this)}>Reset</button>
+    </div>
+  }
+
+  start() {
+    this.subscription = IntervalObservable.create(Constants.PERIOD).subscribe(() => {
+      this.setTime(this.state.time + Constants.INCREMENT);
+    });
+    this.setActive(true);
+  }
+
+  stop() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    this.subscription = null;
+    this.setActive(false);
+  }
+
+  reset() {
+    this.stop();
+    this.setTime(0.0);
+  }
+
+  setTime(time) {
+    this.setState({
+      time
+    });
+  }
+
+  setActive(active) {
+    this.setState({
+      active
+    });
   }
 }
--- src/react/index.jsx
+++ src/react/index.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Stopwatch } from './components/Stopwatch';
-
-// ========================================
+import { Stopwatch } from "./components/Stopwatch";
 
 ReactDOM.render(
   <Stopwatch />,
